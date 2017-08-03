bfcb0d5d95eba9cf8f939617850bd01e62555f41
Andy <andyzanli@gmail.com>
2017-08-03 00:44:57 -0700

    Win animation / Added sounds / Timeouts for alert & showing pattern

--- src/App.js
+++ src/App.js
@@ -24,6 +24,8 @@ class App extends Component {
     this.attempt = [];
     this.strict = false;
     this.showingPattern = false;
+    this.celebrating = false;
+    this.notify = undefined;
     this.errorBorder = '10px solid red';
     this.successBorder = '10px solid green';
     this.normalBorder = '10px solid grey';
@@ -45,9 +47,10 @@ class App extends Component {
     this.blueSound = new Audio('https://s3.amazonaws.com/freecodecamp/simonSound3.mp3');
     this.yellowSound = new Audio('https://s3.amazonaws.com/freecodecamp/simonSound4.mp3');
     this.errorSound = new Audio('./sounds/Computer-Error-Alert.mp3');
+    this.winSound = new Audio('./sounds/Crowd-Applause.mp3');
   }
 
-  render() { console.log(this.memory);
+  render() {
     return (
       <div className={'App'}>
         <Title />
@@ -64,15 +67,14 @@ class App extends Component {
   }
 
   pickNewColor(number) { // 0: red, 1: green, 2: blue, 3: yellow
-    // if (this.memory[this.memory.length - 1] === number) {
-    //   var newNumber = Math.floor(Math.random() * 3);
-    //   while (newNumber === number) {
-    //     newNumber = Math.floor(Math.random() * 3);
-    //   }
-    // }
-    // newNumber ? this.memory.push(newNumber) : this.memory.push(number);
     this.memory.push(number);
     this.updateCount(this.memory.length);
+    this.showPattern();
+  }
+
+  notification() {
+    this.errorSound.play();
+    this.displayErrorSignal();
     this.showPattern();
   }
 
@@ -81,10 +83,12 @@ class App extends Component {
     setTimeout(() => {
       this.showingPattern = false;
     }, this.memory.length * 750);
+    this.notify = setTimeout(() => {
+      this.notification();
+    }, this.memory.length * 750 + 15000);
     var timeout = 0;
     for (let i = 0; i < this.memory.length; i++) {
       setTimeout(() => {
-        console.log(timeout)
         switch (this.memory[i]) {
           case 0:
             if (i === 0) {
@@ -231,17 +235,21 @@ class App extends Component {
 
   handleRedCorner() {
     this.redSound.play();
-    if (this.showingPattern === true) return;
+    if (this.showingPattern || this.celebrating) return;
+    clearTimeout(this.notify);
+    this.notify = setTimeout(() => this.notification, 15000);
     this.setState({0: this.activeRed});
     setTimeout(() => this.setState({0: this.inactiveRed}), 250);
     if (this.memory.length) {
       if (this.attempt.length < this.memory.length) {
         if (this.memory[this.attempt.length] === 0) {
           this.attempt.push(0);
-          if (this.attempt.length === this.memory.length) {
+          if (this.attempt.length === this.memory.length && this.memory.length < 20) {
             this.attempt = [];
             this.displaySuccessSignal();
-            this.pickNewColor(Math.floor(Math.random() * 3));
+            setTimeout(() => this.pickNewColor(Math.floor(Math.random() * 3)), 1000);
+          } else if (this.attempt.length === 20) {
+            this.displayWinSignal();
           }
         } else {
           if (this.state.strict) {
@@ -252,28 +260,30 @@ class App extends Component {
             this.errorSound.play();
             this.displayErrorSignal();
             this.attempt = [];
+            this.showPattern();
           }
         }
-      } else {
-        this.displaySuccessSignal();
-        this.pickNewColor(Math.floor(Math.random() * 3));
       }
     }
   }
 
   handleGreenCorner() {
     this.greenSound.play();
-    if (this.showingPattern === true) return;
+    if (this.showingPattern || this.celebrating) return;
+    clearTimeout(this.notify);
+    this.notify = setTimeout(() => this.notification, 15000);
     this.setState({1: this.activeGreen});
     setTimeout(() => this.setState({1: this.inactiveGreen}), 250);
     if (this.memory.length) {
       if (this.attempt.length < this.memory.length) {
         if (this.memory[this.attempt.length] === 1) {
           this.attempt.push(1);
-          if (this.attempt.length === this.memory.length) {
+          if (this.attempt.length === this.memory.length && this.memory.length < 20) {
             this.attempt = [];
             this.displaySuccessSignal();
-            this.pickNewColor(Math.floor(Math.random() * 3));
+            setTimeout(() => this.pickNewColor(Math.floor(Math.random() * 3)), 1000);
+          } else if (this.attempt.length === 20) {
+            this.displayWinSignal();
           }
         } else {
           if (this.state.strict) {
@@ -284,28 +294,30 @@ class App extends Component {
             this.errorSound.play();
             this.displayErrorSignal();
             this.attempt = [];
+            this.showPattern();
           }
         }
-      } else {
-        this.displaySuccessSignal();
-        this.pickNewColor(Math.floor(Math.random() * 3));
       }
     }
   }
 
   handleBlueCorner() {
     this.blueSound.play();
-    if (this.showingPattern === true) return;
+    if (this.showingPattern || this.celebrating) return;
+    clearTimeout(this.notify);
+    this.notify = setTimeout(() => this.notification, 15000);
     this.setState({2: this.activeBlue});
     setTimeout(() => this.setState({2: this.inactiveBlue}), 250);
     if (this.memory.length) {
       if (this.attempt.length < this.memory.length) {
         if (this.memory[this.attempt.length] === 2) {
           this.attempt.push(2);
-          if (this.attempt.length === this.memory.length) {
+          if (this.attempt.length === this.memory.length && this.memory.length < 20) {
             this.attempt = [];
             this.displaySuccessSignal();
-            this.pickNewColor(Math.floor(Math.random() * 3));
+            setTimeout(() => this.pickNewColor(Math.floor(Math.random() * 3)), 1000);
+          } else if (this.attempt.length === 20) {
+            this.displayWinSignal();
           }
         } else {
           if (this.state.strict) {
@@ -316,28 +328,30 @@ class App extends Component {
             this.errorSound.play();
             this.displayErrorSignal();
             this.attempt = [];
+            this.showPattern();
           }
         }
-      } else {
-        this.displaySuccessSignal();
-        this.pickNewColor(Math.floor(Math.random() * 3));
       }
     }
   }
 
   handleYellowCorner() {
     this.yellowSound.play();
-    if (this.showingPattern === true) return;
+    if (this.showingPattern || this.celebrating) return;
+    clearTimeout(this.notify);
+    this.notify = setTimeout(() => this.notification, 15000);
     this.setState({3: this.activeYellow});
     setTimeout(() => this.setState({3: this.inactiveYellow}), 250);
     if (this.memory.length) {
       if (this.attempt.length < this.memory.length) {
         if (this.memory[this.attempt.length] === 3) {
           this.attempt.push(3);
-          if (this.attempt.length === this.memory.length) {
+          if (this.attempt.length === this.memory.length && this.memory.length < 20) {
             this.attempt = [];
             this.displaySuccessSignal();
-            this.pickNewColor(Math.floor(Math.random() * 3));
+            setTimeout(() => this.pickNewColor(Math.floor(Math.random() * 3)), 1000);
+          } else if (this.attempt.length === 20) {
+            this.displayWinSignal();
           }
         } else {
           if (this.state.strict) {
@@ -348,11 +362,9 @@ class App extends Component {
             this.errorSound.play();
             this.displayErrorSignal();
             this.attempt = [];
+            this.showPattern();
           }
         }
-      } else {
-        this.displaySuccessSignal();
-        this.pickNewColor(Math.floor(Math.random() * 3));
       }
     }
   }
@@ -365,7 +377,7 @@ class App extends Component {
     }
   }
 
-  startNewGame() { console.log('start game')
+  startNewGame() {
     this.memory = [];
     this.attempt = [];
     this.setState({startButton: this.state.startButton + 1});
@@ -381,6 +393,40 @@ class App extends Component {
     setTimeout(() => {
       this.setState({border: this.normalBorder});
     }, 750);
+  }
+
+  displayWinSignal() {
+    this.winSound.play();
+    this.celebrating = true;
+    this.setState({boxShadow: this.redShadow});
+    setTimeout(() => this.setState({boxShadow: this.greenShadow}), 250);
+    setTimeout(() => this.setState({boxShadow: this.blueShadow}), 500);
+    setTimeout(() => this.setState({boxShadow: this.yellowShadow}), 750);
+    setTimeout(() => this.setState({boxShadow: this.redShadow}), 1000);
+    setTimeout(() => this.setState({boxShadow: this.greenShadow}), 1250);
+    setTimeout(() => this.setState({boxShadow: this.blueShadow}), 1500);
+    setTimeout(() => this.setState({boxShadow: this.yellowShadow}), 1750);
+    setTimeout(() => this.setState({boxShadow: this.redShadow}), 2000);
+    setTimeout(() => this.setState({boxShadow: this.greenShadow}), 2250);
+    setTimeout(() => this.setState({boxShadow: this.blueShadow}), 2500);
+    setTimeout(() => this.setState({boxShadow: this.yellowShadow}), 2750);
+    setTimeout(() => this.setState({boxShadow: this.redShadow}), 3000);
+    setTimeout(() => this.setState({boxShadow: this.greenShadow}), 3250);
+    setTimeout(() => this.setState({boxShadow: this.blueShadow}), 3500);
+    setTimeout(() => this.setState({boxShadow: this.yellowShadow}), 3750);
+    setTimeout(() => this.setState({boxShadow: this.redShadow}), 4000);
+    setTimeout(() => this.setState({boxShadow: this.greenShadow}), 4250);
+    setTimeout(() => this.setState({boxShadow: this.blueShadow}), 4500);
+    setTimeout(() => this.setState({boxShadow: this.yellowShadow}), 4750);
+    setTimeout(() => this.setState({boxShadow: this.redShadow}), 5000);
+    setTimeout(() => this.setState({boxShadow: this.greenShadow}), 5250);
+    setTimeout(() => this.setState({boxShadow: this.blueShadow}), 5500);
+    setTimeout(() => this.setState({boxShadow: this.yellowShadow}), 5750);
+    setTimeout(() => this.setState({boxShadow: '0px 0px 50px #e04006, inset 0 0 20px #e04006'}), 6000);
+    setTimeout(() => {
+      this.celebrating = false;
+      this.setState({boxShadow: this.normalShadow, count: '00'});
+    }, 10000);
   }
 
   displayErrorSignal(strict) {
