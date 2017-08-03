d12bd8fb1fab9c1d8c2d2381ffa778f0aebc3250
Andy <andyzanli@gmail.com>
2017-08-02 22:16:19 -0700

    Change box-shadow color when in strict mode.

--- src/components/count/count.js
+++ src/components/count/count.js
@@ -1,13 +1,31 @@
 import React, { Component } from 'react';
 import './count.css';
 
+
 class Count extends Component {
+  constructor() {
+    super();
+    this.state = {
+      boxShadow: 'inset 0 0 4px white',
+    }
+  }
+
   render() {
     return (
-      <div className={'container'}>
+      <div className={'container'} style={{boxShadow: this.state.boxShadow}}>
         <div className={'count'}>{ this.props.count }</div>
       </div>
     );
+  }
+
+  componentWillReceiveProps(nextProps) { console.log(nextProps)
+    if (nextProps.strict !== this.props.strict) {
+      if (nextProps.strict === true) {
+        this.setState({boxShadow: 'inset 0 0 10px #e04006'});
+      } else {
+        this.setState({boxShadow: 'inset 0 0 4px white'});
+      }
+    }
   }
 }
 
