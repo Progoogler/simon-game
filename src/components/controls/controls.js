f5318ef3e3ce8b8c46d2ece710a908d41f464b71
Andy <andyzanli@gmail.com>
2017-08-02 22:15:32 -0700

    Active box-shadow changes when buttons are clicked.

--- src/components/controls/controls.js
+++ src/components/controls/controls.js
@@ -2,17 +2,38 @@ import React, { Component } from 'react';
 import './controls.css';
 
 class Controls extends Component {
+  constructor() {
+    super();
+    this.state = {
+      startShadow: '2px 1px 1px 1px grey',
+      strictShadow: '2px 1px 1px 1px grey',
+    };
+    this.normalShadow = '2px 1px 1px 1px grey';
+    this.pressedShadow = '1px 0px 0px 0px grey';
+  }
+
   render() {
     return (
       <div className={'controls-container'}>
-        <div className={'start-button'} onClick={this.props.startNewGame()}>
+        <div className={'start-button'} style={{boxShadow: this.state.startShadow}} onClick={this.props.startNewGame()}>
           <img src="./images/power-icon.png" alt="Power on/off symbol" />
         </div>
-        <div className={'strict-button'} onClick={this.props.enableStrictMode()}>
+        <div className={'strict-button'} style={{boxShadow: this.state.strictShadow}} onClick={this.props.enableStrictMode()}>
           <img src="./images/flame-icon.png" alt="Flame to signify difficulty" />
         </div>
       </div>
     );
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.start !== this.props.start) {
+      this.setState({startShadow: this.pressedShadow});
+      setTimeout(() => this.setState({startShadow: this.normalShadow}), 250);
+    }
+    if (nextProps.strict !== this.props.strict) {
+      this.setState({strictShadow: this.pressedShadow});
+      setTimeout(() => this.setState({strictShadow: this.normalShadow}), 250);
+    }
   }
 }
 
